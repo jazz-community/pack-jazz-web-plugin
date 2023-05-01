7404cda2612ae31b610e2f1d5dfed4c6b300255f
Martin Benninger <martin.benninger@outlook.com>
2023-05-01 14:10:51 +0200

    Create a zip file with the correct name

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
 "use strict";
 
 const path = require("path");
+const fs = require("fs");
+const archiver = require("archiver");
 const packageJson = require(path.resolve("./package.json"));
+
 const pluginId = packageJson.zipJazzWebPlugin.pluginId;
 const pluginName = packageJson.name;
 const pluginVersion = packageJson.version + "_" + getFormattedDate(new Date());
@@ -12,6 +15,12 @@ const pluginAuthor = packageJson.author;
 const pluginLicense = packageJson.license;
 const pluginFiles = packageJson.zipJazzWebPlugin.pluginFiles;
 const zipFileName = pluginId + "_" + pluginVersion;
+
+const outputFile = fs.createWriteStream(path.resolve("./" + zipFileName + ".zip"));
+const outputArchive = archiver("zip");
+
+outputArchive.pipe(outputFile);
+outputArchive.finalize();
 
 function getFormattedDate(date) {
   return (
