46470ad958f89f1b34c53a2633356130a2dcea82
Martin Benninger <martin.benninger@outlook.com>
2023-05-01 11:55:35 +0200

    Read the properties from the package.json and return the zip file name

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 #!/usr/bin/env node
 
 "use strict";
+
+const path = require("path");
+const packageJson = require(path.resolve("./package.json"));
+const pluginId = packageJson.zipJazzWebPlugin.pluginId;
+const pluginName = packageJson.name;
+const pluginVersion = packageJson.version + "_" + getFormattedDate(new Date());
+const pluginDescription = packageJson.description;
+const pluginAuthor = packageJson.author;
+const pluginLicense = packageJson.license;
+const pluginFiles = packageJson.zipJazzWebPlugin.pluginFiles;
+const zipFileName = pluginId + "_" + pluginVersion;
+
+function getFormattedDate(date) {
+  return (
+    date.getFullYear() +
+    zeroFill(date.getMonth() + 1) +
+    zeroFill(date.getDate()) +
+    "-" +
+    zeroFill(date.getHours()) +
+    zeroFill(date.getMinutes())
+  );
+}
+
+function zeroFill(i) {
+  return (i < 10 ? "0" : "") + i;
+}
+
+process.stdout.write(zipFileName);
